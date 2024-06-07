9153066d3023097dcf7d0d3a87a783dc32f3774c
jodyboy <elite.crew66@gmail.com>
2024-06-08 00:48:41 +0700

    update app front end

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,13 +27,82 @@ class HomeApp extends StatefulWidget {
 }
 
 class _HomeAppState extends State<HomeApp> {
+  // LOGIC
+  int milliseconds = 0, seconds = 0, minutes = 0, hours = 0;
+  String digitMilliseconds = "000", digitSeconds = "00", digitMinutes = "00";
+  Timer? timer;
+  bool started = false;
+  List laps = [];
+
+  //Create stop timer func
+
+  void stop(){
+    timer!.cancel();
+    setState(() {
+      started = false;
+    });
+  }
+
+  //Create the reset function
+
+  void reset(){
+    timer!.cancel();
+    setState(() {
+      milliseconds = 0;
+      seconds = 0;
+      minutes = 0;
+
+      digitMilliseconds = "000";
+      digitSeconds = "00";
+      digitMinutes = "00";
+
+      started = false;
+      laps.clear();
+    });
+  }
+  
+  void addLaps(){
+    String lap = "$digitMinutes:$digitSeconds.$digitMilliseconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  //Create the start timer function
+
+  void start(){
+    started = true;
+    timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      int localMilliseconds = milliseconds + 10;
+      int localSeconds = seconds;
+      int localMinutes = minutes;
+
+      if (localMilliseconds >= 999) {
+        localSeconds++;
+        localMilliseconds = 0;
+      }
+      if (localSeconds >= 59) {
+        localMinutes++;
+        localSeconds = 0;
+      }
+      setState(() {
+        milliseconds = localMilliseconds;
+        seconds = localSeconds;
+        minutes = localMinutes;
+        digitMilliseconds = (milliseconds >= 100) ? "$milliseconds" : (milliseconds >= 10) ? "0$milliseconds" : "00$milliseconds";
+        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1C2757),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(30.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -49,30 +120,113 @@ class _HomeAppState extends State<HomeApp> {
               SizedBox(
                 height: 20.0,
               ),
+              Center(
+                child: Text(
+                  "$digitMinutes:$digitSeconds.$digitMilliseconds",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 80.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
               Container(
-                height: 400.0,
+                height: 240.0,
                 decoration: BoxDecoration(
                   color: Color(0xFF323F68),
                   borderRadius: BorderRadius.circular(8.0),
                 ),
+                // add list builder
+                child: ListView.builder(
+                  itemCount: laps.length,
+                  itemBuilder: (context, index){
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Lap ${index+1}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Text(
+                            "${laps[index]}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ]
+                      ),
+                    );
+                  }                  ),
               ),
               SizedBox(
-                height: 20.0,
-                ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                height: 10.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: RawMaterialButton(
-                      onPressed: () {},
-                      shape: const StadiumBorder(
-                        side: BorderSide(color: Colors.blue),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      RawMaterialButton(
+                        onPressed: () {
+                          (!started) ? start() : stop();
+                        },
+                        fillColor: Colors.blue,
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(80.0),
+                        child: Text(
+                          (!started) ? "START" : "PAUSE",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
                       ),
-                      child: Text(
-                        "Start",
-                        style: TextStyle(color: Colors.white),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40.0
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: RawMaterialButton(
+                          onPressed: () {
+                            reset();
+                          },
+                          shape: const StadiumBorder(
+                            side: BorderSide(color: Colors.blue),
+                          ),
+                          child: Text(
+                            "Reset",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ), 
                       ),
-                      ), 
+                      SizedBox(width: 20.0),
+                      Expanded(
+                        child: RawMaterialButton(
+                          onPressed: () {
+                            addLaps();
+                          },
+                          shape: const StadiumBorder(
+                            side: BorderSide(color: Colors.blue),
+                          ),
+                          child: Text(
+                            "Add Lap",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ), 
+                      ),
+                    ],
                   ),
                 ],
               )
